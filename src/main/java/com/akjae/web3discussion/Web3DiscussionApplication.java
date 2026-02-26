b56ca19369dd3a963d6185b0e2bfa21624cd2171
Dobino <dobino@Dobinoui-Macmini.local>
2026-02-26 03:55:10 -0800

    Initial project setup: Spring Boot + Java + Gradle

---
+++ src/main/java/com/akjae/web3discussion/Web3DiscussionApplication.java
@@ -0,0 +1,12 @@
+package com.akjae.web3discussion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Web3DiscussionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Web3DiscussionApplication.class, args);
+    }
+}
---
+++ src/test/java/com/akjae/web3discussion/Web3DiscussionApplicationTests.java
@@ -0,0 +1,12 @@
+package com.akjae.web3discussion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Web3DiscussionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
